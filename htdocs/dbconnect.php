31a3b2b32ae06d4e1f64c4b4a76bcc0dd3736a94
AviAlao <100569480+AviAlao@users.noreply.github.com>
2022-03-13 17:23:53 +0200

    Add files via upload

---
+++ htdocs/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+//Create connection to the database.
+$host="localhost"; 	// Host name    
+$dbusername="root"; //username   
+$dbpassword=""; 	//username password   
+$dbname="a&a_restaurant"; 	//db name
+      
+// Connect to server and select database.   
+$con = mysqli_connect($host,$dbusername,$dbpassword,$dbname)  
+?>
